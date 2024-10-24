5d671618eec8f8f30cb1f214d978ef6b4d41d73a
Tom Colvin <tom.colvin@apptaura.com>
2024-10-24 11:55:49 +0100

    Refactor and comments, ready for workshop

--- app/src/main/java/com/tdcolvin/gemmallmdemo/ui/reactiongesture/ReactionGestureViewModel.kt
+++ app/src/main/java/com/tdcolvin/gemmallmdemo/ui/reactiongesture/ReactionGestureViewModel.kt
@@ -9,6 +9,7 @@ import com.google.mediapipe.framework.image.BitmapImageBuilder
 import com.google.mediapipe.tasks.core.BaseOptions
 import com.google.mediapipe.tasks.vision.core.RunningMode
 import com.google.mediapipe.tasks.vision.gesturerecognizer.GestureRecognizer
+import com.google.mediapipe.tasks.vision.gesturerecognizer.GestureRecognizerResult
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlin.math.max
@@ -21,33 +22,63 @@ class ReactionGestureViewModel(application: Application): AndroidViewModel(appli
     val uiState = MutableStateFlow(ReactionGestureUiState())
 
     private val gestureRecognizer by lazy {
+        /*
+          TODO: Create a gesture recognizer here and use it to call handleGestureRecognizerResult
+                below, whenever it has a result.
+
+          Hints:
+             * Use the same builder pattern as the last task, but this time using
+               GestureRecognizer.createFromOptions(...)
+
+             * There's an example here:
+               https://ai.google.dev/edge/mediapipe/solutions/vision/gesture_recognizer/android#create_the_task
+               Make sure you click "Live stream" on the code examples!
+
+             * The model asset path is "gesture_recognizer.task"
+
+             * Set the running mode to RunningMode.LIVE_STREAM
+
+             * Because we're in live stream mode, you'll need to set a result listener
+
+             * You will need to run this gesture recogniser - see extra to-do near the end of this
+               file
+
+             * Again, there's a full example on the "answers" branch
+        */
+
         val baseOptionsBuilder = BaseOptions.builder().setModelAssetPath("gesture_recognizer.task")
         val baseOptions = baseOptionsBuilder.build()
 
         val optionsBuilder =
             GestureRecognizer.GestureRecognizerOptions.builder()
                 .setBaseOptions(baseOptions)
-                .setResultListener { result, _ ->
-                    val bestCategory = result.gestures().firstOrNull()?.maxByOrNull { it.score() }
-                    val gesture = when(bestCategory?.categoryName()) {
-                        "Thumb_Up" -> "\uD83D\uDC4D"
-                        "Thumb_Down" -> "\uD83D\uDC4E"
-                        "Pointing_Up" -> "☝\uFE0F"
-                        "Open_Palm" -> "✋"
-                        "Closed_Fist" -> "✊"
-                        "Victory" -> "✌\uFE0F"
-                        "ILoveYou" -> "\uD83E\uDD1F"
-                        else -> null
-                    }
-
-                    if (gesture != null) {
-                        uiState.update { it.copy(mostRecentGesture = gesture) }
-                    }
-                }
+                .setResultListener { result, _ -> handleGestureRecognizerResult(result) }
                 .setRunningMode(RunningMode.LIVE_STREAM)
 
         val options = optionsBuilder.build()
         GestureRecognizer.createFromOptions(getApplication(), options)
+    }
+
+    private fun handleGestureRecognizerResult(result: GestureRecognizerResult) {
+        // Figure out the most likely gesture
+        val bestCategory = result.gestures().firstOrNull()?.maxByOrNull { it.score() }
+
+        // Convert it to an emoji
+        val gesture = when(bestCategory?.categoryName()) {
+            "Thumb_Up" -> "\uD83D\uDC4D"
+            "Thumb_Down" -> "\uD83D\uDC4E"
+            "Pointing_Up" -> "☝\uFE0F"
+            "Open_Palm" -> "✋"
+            "Closed_Fist" -> "✊"
+            "Victory" -> "✌\uFE0F"
+            "ILoveYou" -> "\uD83E\uDD1F"
+            else -> null
+        }
+
+        // Display it in the UI
+        if (gesture != null) {
+            uiState.update { it.copy(mostRecentGesture = gesture) }
+        }
     }
 
     val imageAnalyzer by lazy {
@@ -62,6 +93,7 @@ class ReactionGestureViewModel(application: Application): AndroidViewModel(appli
 
             image.close()
 
+            /* TODO: call the recognizeAsync function of your gesture recognizer here */
             gestureRecognizer.recognizeAsync(BitmapImageBuilder(bmp).build(), System.currentTimeMillis())
         }
     }
